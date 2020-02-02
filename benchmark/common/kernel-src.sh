575d527a8adc55d1f580293207adf817f3904a0c
Nathan Chancellor <natechancellor@gmail.com>
2020-02-02 00:26:03 -0700

    benchmark/common/kernel-src.sh: Update for Linux 5.5
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- benchmark/common/kernel-src.sh
+++ benchmark/common/kernel-src.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 # This hashbang is purely for shellcheck auditing, this script needs to be sourced to work
 
-KERNEL_VER=5.4
+KERNEL_VER=5.5
 KERNEL_DIR=${WORK_DIR:?}/linux-${KERNEL_VER}
+KERNEL_PATCH=${TC_BLD_DIR:?}/kernel/${KERNEL_DIR##*/}-allyesconfig.patch
 (
     cd "${WORK_DIR}" || exit ${?}
     curl -LSs https://cdn.kernel.org/pub/linux/kernel/v5.x/"${KERNEL_DIR##*/}".tar.xz | tar -xJf -
-    patch -d "${KERNEL_DIR}" -p1 < "${TC_BLD_DIR:?}"/kernel/"${KERNEL_DIR##*/}"-allyesconfig.patch
+    if [[ -f ${KERNEL_PATCH} ]]; then
+        patch -d "${KERNEL_DIR}" -p1 < "${KERNEL_PATCH}"
+    fi
 )
